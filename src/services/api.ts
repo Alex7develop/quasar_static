073d13c6c07581dee8bdf576415b020aeb1039b3
Alex7develop <aleksandr@MacBook-Pro-Aleksandr.local>
2025-05-06 22:40:57 +0300

    no_cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ import type {
 
 const API_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://tstai.rasar.keenetic.link'
+    ? 'https://cors-anywhere.herokuapp.com/https://tstai.rasar.keenetic.link'
     : '/api';
 
 const apiClient: AxiosInstance = axios.create({
